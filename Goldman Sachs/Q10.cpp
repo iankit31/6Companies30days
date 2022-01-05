b2c3fc0657a2ce526c456b1ee14b4278e28a02e8
Ankit Pandey <72681667+iankit31@users.noreply.github.com>
2022-01-05 11:56:34 +0530

    Update Q10.cpp

--- Goldman Sachs/Q10.cpp
+++ Goldman Sachs/Q10.cpp
@@ -1,1 +1,35 @@
-//
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    priority_queue<int,vector<int>,greater<int>> pq; 
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+
+    for(int i=0;i<n;i++) 
+        cin>>arr[i];
+    
+    for(int i=0;i<10;i++)
+    {
+      pq.push(arr[i]);
+    }
+    
+    for(int i=10;i<n;i++) 
+    {
+        if(arr[i]>pq.top()) {
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+
+    cout<<"Top 10 elements: out of n numbers are  ";
+    
+    while(!pq.empty()){
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+
+    return 0;
+}
